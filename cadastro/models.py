4301b1c53fe171f6a641246719af767f6ccbc736
Aluno <junior-3c@hotmail.com>
2014-10-21 20:41:36 -0200

    novo

--- cadastro/models.py
+++ cadastro/models.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 from django.db import models
 
 # Create your models here.
@@ -7,18 +8,20 @@ SEXO_OPCOES = [
 
 	('M','Masculino'),
 	('F','Feminino')
-
-class CadastroPessoa (models.Model):
 	
-	Nome = model.Charfield('Nome',max_length=100)
-	Sexo = model.Charfield('Sexo',max_length=2)
-	Cidade = model.Charfield('Cidade',max_lenght=100)
-	Bairro = model.Charfield('Bairro',max_lenght=100)
-	Endereco = model.Charfield('Endereco',max_lenght=100)
-	Numero = model.Charfield('Numero')
-	Telefone = model.Charfield('Telefone',max_lenght=15)
-	Celular = model.Charfield('Celular',max_lenght=15)
-	Email = model.Charfield('Email',max_lenght=100)
+]
+
+class Cadastro (models.Model):
+	
+	Nome = models.CharField('Nome',max_length=100)
+	Sexo = models.CharField('Sexo',max_length=1,choices=SEXO_OPCOES)
+	Cidade = models.CharField('Cidade',max_length=100)
+	Bairro = models.CharField('Bairro',max_length=100)
+	Endereco = models.CharField('Endereco',max_length=100)
+	Numero = models.IntegerField('Numero')
+	Telefone = models.CharField('Telefone',max_length=15)
+	Celular = models.CharField('Celular',max_length=15)
+	Email = models.CharField('Email',max_length=100)
 	
 	
 	def  __unicode__(self):
